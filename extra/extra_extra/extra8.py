05fce3a2352fec284af1bac704762d82ee950e93
nelrinmv <nelrinmv@gmail.com>
2026-06-24 12:03:50 +0530

    Student Information System

--- extra/extra_extra/extra8.py
+++ extra/extra_extra/extra8.py
@@ -1,1 +1,13 @@
+name = input("Enter name: ")
+age = input("Enter age: ")
+clas = input("Enter Class: ")
+percentage = input("Enter Peercentage: ")
 
+studentdetail = {
+    "Name":name,
+    "Age":age,
+    "Class":clas,
+    "Percentage":percentage
+}
+
+print(studentdetail)
