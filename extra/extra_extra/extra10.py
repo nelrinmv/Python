04346071dc0f04e4454133513cd8804e12b8153b
nelrinmv <nelrinmv@gmail.com>
2026-06-24 12:14:49 +0530

    Unique Numbers Finder

--- extra/extra_extra/extra10.py
+++ extra/extra_extra/extra10.py
@@ -1,1 +1,14 @@
+num1 = int(input("Enter Number : "))
+num2 = int(input("Enter Number : "))
+num3 = int(input("Enter Number : "))
+num4 = int(input("Enter Number : "))
+num5 = int(input("Enter Number : "))
+num6 = int(input("Enter Number : "))
+num7 = int(input("Enter Number : "))
+num8 = int(input("Enter Number : "))
 
+num_list = [num1,num2,num3,num4,num5,num6,num7,num8]
+
+newsetlist = set(num_list)
+
+print(newsetlist)
