0dccb93f3a22426aac898d72f27dc3a3618931b3
nelrinmv <nelrinmv@gmail.com>
2026-06-24 09:43:47 +0530

    Q3 of pp5 chapter 5

---
+++ Python Problem/pp5/PP5_Q3.py
@@ -0,0 +1,9 @@
+r1 = int(input("Enter a no: "))
+r2 = int(input("Enter a no: "))
+r3 = input("Enter a no (string): ")
+r4 = int(input("Enter a no: "))
+r5 = int(input("Enter a no: "))
+
+numlist = [r1,r2,r3,r4,r5]
+
+print(set(numlist)) # the output will have both 18 int and 18 str in it 
