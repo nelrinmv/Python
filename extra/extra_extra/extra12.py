3fee21cb5d3dcd0ff8c98ddf439ad159ed307185
nelrinmv <nelrinmv@gmail.com>
2026-06-24 13:00:29 +0530

    Advanced Student RecordAdvanced Student Record

--- extra/extra_extra/extra12.py
+++ extra/extra_extra/extra12.py
@@ -1,1 +1,9 @@
+student = {
+    "name": "Rahul",
+    "marks": [78, 89, 90, 67, 88]
+}
 
+print(student["name"])
+print(max(student["marks"]))
+print(min(student["marks"]))
+print(sum(student["marks"])/len(student["marks"]))
