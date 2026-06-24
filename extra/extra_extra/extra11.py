947109dce9010baa7d594811880bf7488e5a4bca
nelrinmv <nelrinmv@gmail.com>
2026-06-24 12:42:16 +0530

    Dictionary Meaning Lookup

--- extra/extra_extra/extra11.py
+++ extra/extra_extra/extra11.py
@@ -1,1 +1,14 @@
+words = {
+    "python": "Programming Language",
+    "list": "Ordered collection",
+    "tuple": "Immutable collection",
+    "set": "Unordered collection"
+}
 
+wordinput = input("Enter a Word: ")
+
+wordfinder = any(key in wordinput for key in words)
+if wordfinder == True:
+    print(words[wordinput])
+else:
+    print("No word found")
