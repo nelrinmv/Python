3b3f82a3be262d4249ae39edc04ebd46308bb0de
nelrinmv <nelrinmv@gmail.com>
2026-06-24 20:48:05 +0530

    exter problem 80% done

---
+++ extra/extra_extra/extra14.py
@@ -0,0 +1,19 @@
+student = {
+    "name": "Rahul",
+    "Math": 88,
+    "Science": 92,
+    "English": 76,
+    "Computer": 95
+}
+
+print("Highest marks:", max(student["English"],student["Computer"],student["Math"],student["Science"]))
+print("Lowest marks:", min(student["English"],student["Computer"],student["Math"],student["Science"]))
+
+print("Avg marks" , (student["English"]+student["Computer"]+student["Math"]+student["Science"])/(len(student)-1))
+
+marks90 = any(marks >= "90" for marks in student)
+
+if marks90 == True:
+    print("Above 90 in any subject is True")
+else:
+    print("Above 90 marks in any subject is False")
