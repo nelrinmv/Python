66de69bca02e51e9739a6a006daa8eced675b73a
nelrinmv <nelrinmv@gmail.com>
2026-06-24 09:37:58 +0530

    Q2 of pp5 chapter 5

---
+++ Python Problem/pp5/PP5_Q2.py
@@ -0,0 +1,11 @@
+no1 = int(input("Enter a number: "))
+no2 = int(input("Enter a number: "))
+no3 = int(input("Enter a number: "))
+no4 = int(input("Enter a number: "))
+no5 = int(input("Enter a number: "))
+no6 = int(input("Enter a number: "))
+no7 = int(input("Enter a number: "))
+no8 = int(input("Enter a number: "))
+
+noset = [no1,no2,no3,no4,no5,no6,no7,no8]
+print(set(noset))
