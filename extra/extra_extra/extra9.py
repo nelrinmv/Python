6687c4e592ced94e50abe8b9205aaef2a2279233
nelrinmv <nelrinmv@gmail.com>
2026-06-24 12:10:59 +0530

    Favorite Languages Survey

--- extra/extra_extra/extra9.py
+++ extra/extra_extra/extra9.py
@@ -1,1 +1,20 @@
+name1 = input("Enter your name: ")
+lang1 = input("Enter your fav language: ")
 
+name2 = input("Enter your name: ")
+lang2 = input("Enter your fav language: ")
+
+name3 = input("Enter your name: ")
+lang3 = input("Enter your fav language: ")
+
+name4 = input("Enter your name: ")
+lang4 = input("Enter your fav language: ")
+
+name_lang_list = {
+    name1 : lang1 ,
+    name2 : lang2 ,
+    name3 : lang3 ,
+    name4 : lang4
+}
+
+print(name_lang_list)
