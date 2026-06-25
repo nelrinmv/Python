fe39ca5f93044571c68fa42b3c670370ebc402f5
nelrinmv <nelrinmv@gmail.com>
2026-06-25 21:30:50 +0530

    Q1.1 test your python skill 1-11 question done

---
+++ Test python book question/q1.py
@@ -0,0 +1,43 @@
+# Q1.1
+print("Thank You God!") #Output: Thank You God!
+
+# Q1.2
+x = "Pinky"
+print("Believe in yourself", x) #Output: Believe in yourself Pinky
+
+# Q1.3
+print("3 raised to power 4 = ", 3**4 ) #Output: 3 raised to power 4 =  81
+
+# Q1.4
+m = 10
+n = 20
+print(m, "+", n, "=", m+n) #Output: 10 + 20 = 30
+
+# Q1.5
+print(2+4,6/3,90*8) #Output: 6 2 720
+
+# Q1.6
+x = input() #Input: 12
+print(x) #Output: the value entered by the user here 12
+
+# Q1.7
+x1 = int(input("Enter an integer number ")) #Input: 23
+print(x1) #Output:The value entered by the user here 23
+
+# Q1.8
+x = input() #Input: 12
+x1 = int(input('Enter an integer number')) # input: 23
+# print(x1 + x) #Output: Error
+
+# Q1.9
+val = int(input('Enter a floating value: ')) #input: 23.7
+# Output : Error
+
+# Q1.10
+int1 = float(input('Enter a floating value: ')) #input: 23
+print(int1) #Output: 23.0
+
+# Q1.11
+import string
+for i in range(5):
+    print(repr(i).rjust(5))
