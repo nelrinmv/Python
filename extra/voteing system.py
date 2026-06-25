07d9dc16ecef62f1cfd38de1941a5bf4227fda0b
nelrinmv <nelrinmv@gmail.com>
2026-06-25 11:27:22 +0530

    Voteing System

---
+++ extra/voteing system.py
@@ -0,0 +1,26 @@
+lang1 = input("Enter your vote: ")
+lang2 = input("Enter your vote: ")
+lang3 = input("Enter your vote: ")
+lang4 = input("Enter your vote: ")
+lang5 = input("Enter your vote: ")
+lang6 = input("Enter your vote: ")
+
+lang1.capitalize()
+lang2.capitalize()
+lang3.capitalize()
+lang4.capitalize()
+lang5.capitalize()
+lang6.capitalize()
+
+votelistv1 = [lang1,lang2,lang3,lang4,lang5,lang6]
+
+uniquelangset = set(votelistv1)
+print("Unique Language Voted:\n",uniquelangset)
+
+print("Total Votes:",len(votelistv1))   
+print("Total Unique Votes",len(uniquelangset))   
+
+if len(uniquelangset) == 1 :
+    print("Every One voted for the same language.")
+else:
+    print("No One voted for the same language.")
