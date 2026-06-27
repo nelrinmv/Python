2d94d0a80bc248eedc41145e857e5b63ee2b88bf
nelrinmv <nelrinmv@gmail.com>
2026-06-27 11:32:32 +0530

    Main menu for project 3 movie recommendation v1

---
+++ Projects/MovieRecommendation.py
@@ -0,0 +1,10 @@
+print("Hello welecome to the Movie Recommendation Program")
+print("[1] Would you like to continue with the recommendation or")
+print("[2] Would you like to end the program.")
+
+user_input = input("Choose the option that you want to continue with: ")
+
+if user_input == "1":
+    print("Loading program... \n")
+else:
+    print("Thank you for visiting the program do come again later")
