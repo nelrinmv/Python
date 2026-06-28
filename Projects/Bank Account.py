e1bc72aec402b431cfd2f38d69e54d37dc814ce6
nelrinmv <nelrinmv@gmail.com>
2026-06-28 15:42:08 +0530

    bank account project completed v1

--- Projects/Bank Account.py
+++ Projects/Bank Account.py
@@ -1,10 +1,9 @@
 bankacc = {
     "Name" : "Nelrin",
     "accno" : 31425412523,
-    "balance" : 176390,
+    "balance" : 255000,
     "phoneno" : 9053503045
 }
-
 
 print("[1] Check Balance")
 print("[2] Deposit Money")
@@ -15,11 +14,19 @@ user_input = input("\nPlease type the number of the option that you want to cont
 
 if user_input == "1":
     print("Loading Bank Balance...")
+    print("\nBank balance is:", bankacc["balance"])
 elif user_input == "2":
-    print("Loading program for Deposit of money...")
-elif user_input == "3":
-    print("Loading program for Withdraw of money...")
+    print("Loading program for Deposit of money...\n")
+    newbalance = bankacc["balance"] + int(input("Please enter the amount that you have deposited: "))
+    bankacc.update({"balance" : newbalance})
+    print("The account current balance is: Rs.",bankacc["balance"])
+elif user_input == "3" and bankacc["balance"] > 0:
+    print("Loading program for Withdraw of money...\n")
+    newbalance = bankacc["balance"] - int(input("Please enter the amount that you want to withdraw: "))
+    bankacc.update({"balance" : newbalance})
+    print("The account current balance is: Rs.",bankacc["balance"])
 elif user_input == "4":
-    print("Loading Account Details...")
+    print("Loading Account Details...\n")
+    print(bankacc)
 else:
-    print("!!ERROR!! ..The option entered by you iss irrelevent please try again later.. !!ERROR!!")
+    print("!!ERROR!! ..The option entered by you is irrelevent please try again later or the balance in the account is 0 or less.. !!ERROR!!")
