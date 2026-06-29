674db80c80180bf37ce4b53e2efd49979ad4b9ec
nelrinmv <nelrinmv@gmail.com>
2026-06-29 20:58:08 +0530

    pp6 q1

---
+++ Python Problem/pp6/PP6_Q1.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a Number: "))
+num2 = int(input("Enter a Number: "))
+num3 = int(input("Enter a Number: "))
+num4 = int(input("Enter a Number: "))
+
+number = [num1,num2,num3,num4]
+print("The greatest number amoung the digits that the user entered is:",max(number))
