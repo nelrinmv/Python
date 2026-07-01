a729e8abf33bbf48324858ee5a57b3e80cdfc5cc
nelrinmv <nelrinmv@gmail.com>
2026-07-01 17:38:27 +0530

    Chapter 7 and pp7  q1,2

---
+++ Chapter7.py
@@ -0,0 +1,14 @@
+i = 0
+
+# while i < 5:
+#     print("The value of i is: ", i)
+#     print("Hello World")
+#     i = i + 1
+
+for i in range(4):
+    print("printing")
+    if i == 2: # if i is 2, the iteration is skipped
+        continue
+    print(i)
+
+    
---
+++ Python Problem/pp7/PP7_Q1.py
@@ -0,0 +1,4 @@
+user_input = int(input("Enter the Number: "))
+
+for i in range(1,11):
+    print(user_input * i)
