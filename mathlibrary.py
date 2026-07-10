9cc555c9535a90f455466dbc075a3a9c1bb9b917
nelrinmv <nelrinmv@gmail.com>
2026-07-10 20:33:02 +0530

    parts that we can import from the math library

---
+++ mathlibrary.py
@@ -0,0 +1,29 @@
+# Constants
+from math import pi, e, tau, inf, nan
+
+# Basic Arithmetic & Rounding
+from math import ceil, floor, trunc, fabs, fmod, remainder, modf, fma
+
+# Number Theory, Combinatorics & Integer Operations
+from math import gcd, lcm, factorial, comb, perm, isqrt
+
+# Power, Roots, & Logarithms
+from math import sqrt, cbrt, pow, exp, exp2, expm1, log, log10, log2, log1p
+
+# Summation, Products & Vector Geometry
+from math import fsum, prod, sumprod, dist, hypot
+
+# Angular Conversion & Trigonometry
+from math import degrees, radians, sin, cos, tan, asin, acos, atan, atan2
+
+# Hyperbolic Functions
+from math import sinh, cosh, tanh, asinh, acosh, atanh
+
+# Special Advanced Mathematical Functions
+from math import erf, erfc, gamma, lgamma
+
+# Floating-Point Manipulations (Advanced/Low-Level)
+from math import copysign, frexp, ldexp, nextafter, ulp
+
+# State Classification & Precision Matching
+from math import isclose, isfinite, isinf, isnan
