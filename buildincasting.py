eae92c3b00a0b724b07197d3d0de6647c493a687
nelrinmv <nelrinmv@gmail.com>
2026-07-11 09:41:09 +0530

    Built in casting

---
+++ buildincasting.py
@@ -0,0 +1,23 @@
+# Text / String Casting
+from builtins import str, repr, ascii
+
+# Primitive Numeric Casting
+from builtins import int, float, complex
+
+# Boolean / Truth-Value Casting
+from builtins import bool
+
+# Sequence & Iterable Casting
+from builtins import list, tuple, set, frozenset
+
+# Key-Value Mapping Casting
+from builtins import dict
+
+# Binary & Byte-Level Casting
+from builtins import bytes, bytearray, memoryview
+
+# Character Code Casting
+from builtins import chr, ord
+
+# Numeric System Bases
+from builtins import bin, oct, hex
